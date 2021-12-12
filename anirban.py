647035c00f05628b6a30c62a15b23cb82aa9c6a8
anirbanbhattacharyea <95526478+anirbanbhattacharyea@users.noreply.github.com>
2021-12-12 15:19:19 +0530

    Vowel counter.py

---
+++ anirban.py
@@ -0,0 +1,7 @@
+n=input("enter a word ?")
+c=0
+for i in n:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
+        c=c+1
+
+print ("no. of vowels-",c)
